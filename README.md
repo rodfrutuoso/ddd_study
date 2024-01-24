b121bba982909631d6f10e59ad9f124f331358e3
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2024-01-24 16:05:06 -0300

    entity photos

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ Há 4 acamadas Infraestrutura, Adaptadores de Interface, Casos de Uso e Entidade
 # TODO
 [X] Criar caso de uso para encontrar shift por supervisor e contrato utilizando repositórios de equipes
 [ ] No index da equipe enviar objeto para coor, sup e enc com nome e id
-[ ] Falta criar as entidades de casos de uso dos colaborador por turno
+[X] Falta criar as entidades de casos de uso dos colaborador por turno
 [ ] Falta criar as entidades de casos de uso das fotos
 [ ] Falta criar as entidades de casos de uso da programação
 [X] Falta criar as entidades de casos do supervisor
---
+++ src/domain/utils/enterprise/entities/photos.ts
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+
+export interface PhotoProps {
+  type?: string;
+  typeId?: UniqueEntityId;
+  shiftId: UniqueEntityId;
+  linkDrive: string;
+}
+
+export class Photo extends Entity<PhotoProps> {
+  get type() {
+    return this.props.type;
+  }
+
+  get typeId() {
+    return this.props.typeId;
+  }
+
+  get shiftId() {
+    return this.props.shiftId;
+  }
+
+  get linkDrive() {
+    return this.props.linkDrive;
+  }
+
+  static create(props: PhotoProps, id?: UniqueEntityId) {
+    const photo = new Photo(
+      {
+        ...props,
+      },
+      id
+    );
+
+    return photo;
+  }
+}
