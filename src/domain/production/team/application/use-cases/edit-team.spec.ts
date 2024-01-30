4cc32d4a050998e8cb07c7c9fe6ac64bfb33559c
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2024-01-30 16:04:13 -0300

    edit team ajust

--- src/domain/production/team/application/use-cases/edit-team.spec.ts
+++ src/domain/production/team/application/use-cases/edit-team.spec.ts
@@ -3,14 +3,32 @@ import { EditTeam } from "./edit-team";
 import { InMemoryTeamRepository } from "test/repositories/in-memory-team-repository";
 import { makeTeam } from "test/factories/make-team";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { InMemoryTeamLeaderRepository } from "test/repositories/in-memory-teamLeader-repository";
+import { InMemorySupervisorRepository } from "test/repositories/in-memory-supervisor-repository";
+import { InMemoryCoordinatorRepository } from "test/repositories/in-memory-coordinator-repository";
+import { makeTeamLeader } from "test/factories/make-teamLeader";
+import { makeSupervisor } from "test/factories/make-supervisor";
+import { makeCoordinator } from "test/factories/make-coordinator";
+import { UserNameId } from "@/core/entities/userNameId";
 
 let inMemoryTeamRepository: InMemoryTeamRepository;
+let inMemoryTeamLeaderRepository: InMemoryTeamLeaderRepository;
+let inMemorySupervisorRepository: InMemorySupervisorRepository;
+let inMemoryCoordinatorRepository: InMemoryCoordinatorRepository;
 let sut: EditTeam; // system under test
 
 describe("Edit Team By Id", () => {
   beforeEach(() => {
     inMemoryTeamRepository = new InMemoryTeamRepository();
-    sut = new EditTeam(inMemoryTeamRepository);
+    inMemoryTeamLeaderRepository = new InMemoryTeamLeaderRepository();
+    inMemorySupervisorRepository = new InMemorySupervisorRepository();
+    inMemoryCoordinatorRepository = new InMemoryCoordinatorRepository();
+    sut = new EditTeam(
+      inMemoryTeamRepository,
+      inMemorySupervisorRepository,
+      inMemoryTeamLeaderRepository,
+      inMemoryCoordinatorRepository
+    );
   });
 
   it("should be albe to edit a team by its id", async () => {
@@ -52,5 +70,37 @@ describe("Edit Team By Id", () => {
         deactivation_date: new Date(),
       });
     }).rejects.toBeInstanceOf(Error);
+  });
+
+  it("should be albe to edit the supervisor, leader and coordinator team by its id", async () => {
+    const leader = await makeTeamLeader();
+    await inMemoryTeamLeaderRepository.create(leader);
+
+    const supervisor = await makeSupervisor();
+    await inMemorySupervisorRepository.create(supervisor);
+
+    const coordinator = await makeCoordinator();
+    await inMemoryCoordinatorRepository.create(coordinator);
+
+    const newTeam = await makeTeam(
+      { name: "ECOLM001" },
+      new UniqueEntityId("abc-123-xyz")
+    );
+
+    await inMemoryTeamRepository.create(newTeam);
+
+    await sut.execute({
+      teamId: newTeam.id.toString(),
+      programmerType: "PROGRAMAÇÃO",
+      supervisorId: supervisor.id.toString(),
+      leaderId: leader.id.toString(),
+      coordinatorId: coordinator.id.toString(),
+    });
+
+    expect(await inMemoryTeamRepository.items[0]).toMatchObject({
+      supervisorId: new UserNameId(supervisor.name, supervisor.id),
+      leaderId: new UserNameId(leader.name, leader.id),
+      coordinatorId: new UserNameId(coordinator.name, coordinator.id),
+    });
   });
 });
--- src/domain/production/team/application/use-cases/edit-team.ts
+++ src/domain/production/team/application/use-cases/edit-team.ts
@@ -2,7 +2,10 @@
 /* eslint-disable camelcase */
 import { TeamRepository } from "../repositories/team-repository";
 import { Team } from "../../enterprise/entities/team";
-import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { UserNameId } from "@/core/entities/userNameId";
+import { SupervisorRepository } from "@/domain/users/application/repositories/supervisor-repository";
+import { TeamLeaderRepository } from "@/domain/users/application/repositories/teamLeader-repository";
+import { CoordinatorRepository } from "@/domain/users/application/repositories/coordinator-repository";
 
 interface EditTeamInterfaceRequest {
   teamId: string;
@@ -20,7 +23,12 @@ interface EditTeamInterfaceResponse {
 }
 
 export class EditTeam {
-  constructor(private teamRepository: TeamRepository) {}
+  constructor(
+    private teamRepository: TeamRepository,
+    private supervisorRepository: SupervisorRepository,
+    private teamleaderRepository: TeamLeaderRepository,
+    private coordinatorRepository: CoordinatorRepository
+  ) {}
 
   async execute({
     teamId,
@@ -38,11 +46,39 @@ export class EditTeam {
     if (programmerType !== "ADM" && programmerType !== "PROGRAMAÇÃO")
       throw new Error("Not authorized");
 
+    // verifying if teamLeader, Supervisor and coordinator Id's are valid and geting their names
+    let supervisor;
+    if (supervisorId) {
+      supervisor = await this.supervisorRepository.findById(supervisorId);
+      if (!supervisor) throw new Error("Supervisor not found");
+    }
+
+    let coordinator;
+    if (coordinatorId) {
+      coordinator = await this.coordinatorRepository.findById(coordinatorId);
+      if (!coordinator) throw new Error("Coordinator not found");
+    }
+
+    let teamLeader;
+    if (leaderId) {
+      teamLeader = await this.teamleaderRepository.findById(leaderId);
+      if (!teamLeader) throw new Error("Team Leader not found");
+    }
+
     team.name = name ?? team.name;
-    team.leaderId = new UniqueEntityId(leaderId) ?? team.leaderId;
-    team.supervisorId = new UniqueEntityId(supervisorId) ?? team.supervisorId;
+    team.leaderId =
+      leaderId === undefined
+        ? team.leaderId
+        : new UserNameId(teamLeader?.name, teamLeader?.id);
+    team.supervisorId =
+      supervisorId === undefined
+        ? team.supervisorId
+        : new UserNameId(supervisor?.name, supervisor?.id);
     // eslint-disable-next-line prettier/prettier
-    team.coordinatorId = new UniqueEntityId(coordinatorId) ?? team.coordinatorId;
+    team.coordinatorId =
+      coordinatorId === undefined
+        ? team.coordinatorId
+        : new UserNameId(coordinator?.name, coordinator?.id);
     if (deactivation_date !== undefined) team.deactivation_date = new Date();
 
     await this.teamRepository.save(team);
